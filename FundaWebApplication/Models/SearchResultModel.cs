c50cfbede14ec31b5a2e5bf29ae83d62f81b5062
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 03:52:03 +0200

    Add properties comments for search results

--- FundaWebApplication/Models/SearchResultModel.cs
+++ FundaWebApplication/Models/SearchResultModel.cs
@@ -4,7 +4,13 @@ namespace FundaWebApplication.Models
 {
     public class SearchResultModel
     {
+        /// <summary>
+        /// A list of properties for the current search result
+        /// </summary>
         public List<PropertyModel> Objects { get; set; }
+        /// <summary>
+        /// The pagination details for the current search result
+        /// </summary>
         public PagingModel Paging { get; set; }
     }
 }
