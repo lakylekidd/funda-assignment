5f26ca9718e8c19b4882a812a0841d5c0688a5c0
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 15:35:47 +0200

    Add top agents with gardens

--- FundaReactClient/ClientApp/src/components/TopTenAgentsTuin.jsx
+++ FundaReactClient/ClientApp/src/components/TopTenAgentsTuin.jsx
@@ -1,10 +1,38 @@
 ﻿import React, { Component } from 'react';
+import AgentTable from './AgentTable';
+import * as request from 'superagent';
 
 export default class TopTenAgentsTuin extends Component {
+	state = {
+		agents: []
+	};
+
+	loadTopAgents = () => {
+		// Request top ten agents
+		request
+			.get('http://localhost:60398/api/funda/top10withtuin')
+			.then((result) => {
+				// Check if Result is successful
+				if (result.status === 200) {
+					// Assign the agents to the state
+					this.setState({
+						agents: result.body
+					});
+				}
+			})
+			.catch(console.error);
+	};
+
+	componentDidMount() {
+		// Start loading the agents
+		this.loadTopAgents();
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Top 10 Agents with most properties that include a garden</h1>
+				<AgentTable agents={this.state.agents} />
 			</div>
 		);
 	}
