d2c68c82da0a3e1ac265065a5a072fc7a4ca3679
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 03:46:18 +0200

    Add a property model

---
+++ FundaWebApplication/Models/PropertyModel.cs
@@ -0,0 +1,26 @@
+﻿namespace FundaWebApplication.Models
+{
+    public class PropertyModel : AgencyModel
+    {
+        /// <summary>
+        /// The id of the listing
+        /// </summary>
+        public int GlobalId { get; set; }
+        /// <summary>
+        /// Determines whether or not the property has been rented
+        /// </summary>
+        public bool IsVerhuurd { get; set; }
+        /// <summary>
+        /// Determines whether or not the property has been bought
+        /// </summary>
+        public bool IsVerkocht { get; set; }
+        /// <summary>
+        /// Determines whether or not the property has been bought or rented
+        /// </summary>
+        public bool IsVerkochtOfVerhuurd { get; set; }
+        /// <summary>
+        /// The total price of the property
+        /// </summary>
+        public int KoopprijsTot { get; set; }
+    }
+}
--- FundaWebApplication/Models/SearchResultModel.cs
+++ FundaWebApplication/Models/SearchResultModel.cs
@@ -1,7 +1,10 @@
-﻿namespace FundaWebApplication.Models
+﻿using System.Collections.Generic;
+
+namespace FundaWebApplication.Models
 {
     public class SearchResultModel
     {
+        public List<PropertyModel> Objects { get; set; }
         public PagingModel Paging { get; set; }
     }
 }
