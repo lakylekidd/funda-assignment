78227e4350ed94f05dcd46239ac82ce5e60e5b59
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 02:34:02 +0200

    Include base url and key along with constructor for the service

--- FundaWebApplication/Services/FundaService.cs
+++ FundaWebApplication/Services/FundaService.cs
@@ -8,6 +8,13 @@ namespace FundaWebApplication.Services
     public class FundaService : IFundaService
     {
         private readonly HttpClient _client = new HttpClient();
+        private readonly string baseurl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/";
+        private readonly string key = "ac1b0b1572524640a0ecc54de453ea9f";
+
+        public FundaService()
+        {
+
+        }
 
         public async Task<IEnumerable<string>> Get()
         {
