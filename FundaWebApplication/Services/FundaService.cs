ac40302c7e0327a8bad55a7214e210dc3f5ebc1c
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 02:40:37 +0200

    Remove boilerplate from index and fix title

--- FundaWebApplication/Services/FundaService.cs
+++ FundaWebApplication/Services/FundaService.cs
@@ -7,9 +7,8 @@ namespace FundaWebApplication.Services
 {
     public class FundaService : IFundaService
     {
-        private readonly HttpClient _client = new HttpClient();
-        private readonly string baseurl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/";
-        private readonly string key = "ac1b0b1572524640a0ecc54de453ea9f";
+        private readonly string _baseurl = "http://partnerapi.funda.nl/feeds/Aanbod.svc/json/";
+        private readonly string _key = "ac1b0b1572524640a0ecc54de453ea9f";
 
         public FundaService()
         {
