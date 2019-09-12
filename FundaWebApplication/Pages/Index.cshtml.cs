e65b4ece0bba6d8fd3b266b7ee6ce03a09bf9f74
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 02:43:06 +0200

    Add funda service to constructor via DI

--- FundaWebApplication/Pages/Index.cshtml.cs
+++ FundaWebApplication/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FundaWebApplication.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,13 @@ namespace FundaWebApplication.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IFundaService _fundaService;
+
+        public IndexModel(IFundaService fundaService)
+        {
+            _fundaService = fundaService;
+        }
+
         public void OnGet()
         {
 
