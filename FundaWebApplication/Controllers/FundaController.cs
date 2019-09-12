930fa553f7e3c24d6b8c0fc07452737d8acd93e2
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 14:14:45 +0200

    Implemente gateway for top ten agencies with properties that include tuin

--- FundaWebApplication/Controllers/FundaController.cs
+++ FundaWebApplication/Controllers/FundaController.cs
@@ -37,5 +37,25 @@ namespace FundaWebApplication.Controllers
                 return StatusCode(500, new  { error = "Unable to retrieve top 10 agents at the moment. Try again later."});
             }
         }
+
+        [HttpGet("top10withtuin")]
+        public async Task<ActionResult<IEnumerable<ResultByAgencyModel>>> GetTop10WithTuin()
+        {
+            try
+            {
+                // Retrieve a list of agencies with their properties that contain a yard
+                // Node: These agencies already come ordered by number of properties desc
+                var agencies = await _fundaService.GetAgenciesWithMostPropertiesForSale("/amsterdam/tuin");
+                // Retrieve the top 10
+                var top10Agencies = agencies.Take(10).ToList();
+                // Return success
+                return Ok(top10Agencies);
+            }
+            catch
+            {
+                // In case of any error, return 500 and inform caller about 
+                return StatusCode(500, new { error = "Unable to retrieve top 10 agents at the moment. Try again later." });
+            }
+        }
     }
 }
