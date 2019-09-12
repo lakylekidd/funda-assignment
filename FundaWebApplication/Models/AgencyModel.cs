7f3cd452d668aaddd1bfdfc3ce709caf7a7daaeb
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 03:51:24 +0200

    Add agency properties comments

--- FundaWebApplication/Models/AgencyModel.cs
+++ FundaWebApplication/Models/AgencyModel.cs
@@ -2,7 +2,13 @@
 {
     public class AgencyModel
     {
+        /// <summary>
+        /// The agency id
+        /// </summary>
         public int MakelaarId { get; set; }
+        /// <summary>
+        /// The agency name
+        /// </summary>
         public string MakelaarNaam { get; set; }
     }
 }
