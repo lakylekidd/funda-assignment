bcee5490377a476ad0cac975ae6cd7ac44a0fb66
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 04:03:28 +0200

    Create result by agency model

---
+++ FundaWebApplication/Models/ResultByAgencyModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace FundaWebApplication.Models
+{
+    /// <summary>
+    /// A class that specifies an agency with a list of <see cref="PropertyModel"/>
+    /// </summary>
+    public class ResultByAgencyModel : AgencyModel
+    {
+        /// <summary>
+        /// A list of properties for current agency
+        /// </summary>
+        public List<PropertyModel> Properties { get; set; }
+    }
+}
