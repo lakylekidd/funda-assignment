a96c0c9857789f6483c5123135cad21123dd232b
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 15:32:23 +0200

    Add description in home page

--- FundaReactClient/ClientApp/src/components/Home.js
+++ FundaReactClient/ClientApp/src/components/Home.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+	static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h1>Hey there! &#128521;</h1>
+				<p>
+					Welcome to my <a href="https://www.funda.nl/en/">Funda</a> assignment!
+				</p>
+				<p>
+					I hope you like my implementation of the requirements for this assignment. I know that I should not
+					spend more than 4 hours on this assignment and hopefully I have implemented enough to show a part of
+					my skills in C#, JavaScript and CSS.
+				</p>
+				For this client application I used the following technologies:
+				<p />
+				<ul>
+					<li>ASP.NET Core 2.2</li>
+					<li>React.js</li>
+					<li>CSS</li>
+					<li>Superagent</li>
+				</ul>
+				<p>
+					Please have a look around the site as there are two pages where you can see a list of Agencies
+					ranked by number of properties as requested.
+				</p>
+				<q>
+					<b>Please Note:</b> due to the heave request operations on the API, it may take some time to load
+					the data.
+				</q>
+				<br />
+				<br />
+				<p>Thank you for considering my application! &#10084;</p>
+				<a href="https://linkedin.com/in/billy-vlachos/">Billy Vlachos</a>
+			</div>
+		);
+	}
 }
