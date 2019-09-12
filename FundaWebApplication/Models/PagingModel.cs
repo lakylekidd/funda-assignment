1cbe219511ee3e50474a1dcb22a7343826bfaa41
Billy Vlachos <williamcaminiti@live.com>
2019-09-12 03:56:48 +0200

    Include paging model class properties information

--- FundaWebApplication/Models/PagingModel.cs
+++ FundaWebApplication/Models/PagingModel.cs
@@ -5,9 +5,21 @@
     /// </summary>
     public class PagingModel
     {
+        /// <summary>
+        /// Determines the total pages of the search results
+        /// </summary>
         public int AantalPaginas { get; set; }
+        /// <summary>
+        /// Determines the current page of the search results
+        /// </summary>
         public int HuidigePagina { get; set; }
+        /// <summary>
+        /// Determines the next url to be called by pagination script
+        /// </summary>
         public string VolgendeUrl { get; set; }
+        /// <summary>
+        /// Determines the previous url to be called by pagination script
+        /// </summary>
         public string VorigeUrl { get; set; }
     }
 }
